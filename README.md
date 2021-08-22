86a5576da3635bd68deb5203b780abfff529d59c
hesham <17p6020@eng.asu.edu.eg>
2021-08-22 21:49:15 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-English-Football-Python-Data-Analysis
+# YouTube Video:
+
+https://youtu.be/PBK8PzSx2ro
+
+## Python Premier League Data Analysis
+
+![Alt text](result/0.jpg)
+
+![Alt text](result/1.jpg)
+
+![Alt text](result/2.jpg)
+
+![Alt text](result/3.jpg)
+
+![Alt text](result/4.jpg)
+
+![Alt text](result/5.jpg)
+
+![Alt text](result/6.jpg)
+
+![Alt text](result/7.jpg)
+
+![Alt text](result/8.jpg)
+
+![Alt text](result/9.jpg)
+
+![Alt text](result/10.jpg)
+
+![Alt text](result/11.jpg)
+
+![Alt text](result/12.jpg)
+
+![Alt text](result/13.jpg)
+
+![Alt text](result/14.jpg)
+
+![Alt text](result/15.jpg)
+
--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas
+import Functions
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+first_decade = pandas.read_csv('2000-2009.csv')
+second_decade = pandas.read_csv('2010-2019.csv')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Title Winners (pie chart)
+Functions.winning_clubs(data=first_decade, title='2000 - 2009 Winners')
+Functions.winning_clubs(data=second_decade, title='2010 - 2019 Winners')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# -----------------------------------------------------------------------------
+
+# Top Teams Qualified For UCL (barplot)
+Functions.champions_league_clubs(data=first_decade, title='2000 - 2009 UCL')
+Functions.champions_league_clubs(data=second_decade, title='2010 - 2019 UCL')
+
+# Top Teams Relegated (barplot)
+Functions.worst_clubs(data=first_decade, title='2000 - 2009 Relegation')
+Functions.worst_clubs(data=second_decade, title='2010 - 2019 Relegation')
+
+# ------------------------------------------------------------
+
+# Top Teams In Win Matches (barplot)
+Functions.most_in(data=first_decade, series='won', title='2000 - 2009 Wins')
+Functions.most_in(data=second_decade, series='won', title='2010 - 2019 Wins')
+
+# Top Teams In Draw Matches (barplot)
+Functions.most_in(data=first_decade, series='draw', title='2000 - 2009 Draws')
+Functions.most_in(data=second_decade, series='draw', title='2010 - 2019 Draws')
+
+# Top Teams In Lose Matches (barplot)
+Functions.most_in(data=first_decade, series='lost', title='2000 - 2009 Loses')
+Functions.most_in(data=second_decade, series='lost', title='2010 - 2019 Loses')
+
+# Top Teams In Goal For (barplot)
+Functions.most_in(data=first_decade, series='gf', title='2000 - 2009 Goal For')
+Functions.most_in(data=second_decade, series='gf', title='2010 - 2019 Goal For')
+
+# Top Teams In Goal Against (barplot)
+Functions.most_in(data=first_decade, series='ga', title='2000 - 2009 Goal Against')
+Functions.most_in(data=second_decade, series='ga', title='2010 - 2019 Goal Against')
+
+# Top Teams In Points (barplot)
+Functions.most_in(data=first_decade, series='points', title='2000 - 2009 Points')
+Functions.most_in(data=second_decade, series='points', title='2010 - 2019 Points')
+
+# -----------------------------------------------------------------------------
+
+# Teams Performance Due To Points
+Functions.team_performance(data=first_decade, team='Manchester United', title='Manchester United 2000-2009')
+Functions.team_performance(data=second_decade, team='Manchester United', title='Manchester United 2010-2019')
+
+Functions.team_performance(data=first_decade, team='Manchester City', title='Manchester City 2000-2009')
+Functions.team_performance(data=second_decade, team='Manchester City', title='Manchester City 2010-2019')
+
+Functions.team_performance(data=first_decade, team='Chelsea', title='Chelsea 2000-2009')
+Functions.team_performance(data=second_decade, team='Chelsea', title='Chelsea 2010-2019')
+
+Functions.team_performance(data=first_decade, team='Liverpool', title='Liverpool 2000-2009')
+Functions.team_performance(data=second_decade, team='Liverpool', title='Liverpool 2010-2019')
+
+Functions.team_performance(data=first_decade, team='Arsenal', title='Arsenal 2000-2009')
+Functions.team_performance(data=second_decade, team='Arsenal', title='Arsenal 2010-2019')
+
+Functions.team_performance(data=first_decade, team='Tottenham Hotspur ', title='Tottenham Hotspur 2000-2009')
+Functions.team_performance(data=second_decade, team='Tottenham Hotspur ', title='Tottenham Hotspur 2010-2019')
